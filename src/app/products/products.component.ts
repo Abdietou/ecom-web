e6295000b2b6a71cff303bd7fd2d83063d92a8b9
Abdietou <a.doucoure@ymail.com>
2020-06-30 20:01:50 +0200

    upload de photo vers le serveur + appelle ws produit promotion et dispo

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {CatalogueService} from '../catalogue.service';
 import {ActivatedRoute, NavigationEnd, Router} from '@angular/router';
+import {HttpEventType, HttpResponse} from '@angular/common/http';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-products',
@@ -9,8 +11,14 @@ import {ActivatedRoute, NavigationEnd, Router} from '@angular/router';
 })
 export class ProductsComponent implements OnInit {
   private products;
+  private editPhoto: boolean;
+  private currentProduct: any;
+  private selectedFiles;
+  private progress: number;
+  private currentFileUpload;
+  private title:string;
 
-  constructor(private catService: CatalogueService, private route:ActivatedRoute, private router:Router) {}
+  constructor(private catService: CatalogueService, private route:ActivatedRoute, private router:Router, private toastr: ToastrService) {}
 
   ngOnInit() {
     this.router.events.subscribe(params=> {
@@ -19,10 +27,21 @@ export class ProductsComponent implements OnInit {
         console.log(url);
         let paramsId = this.route.snapshot.params.paramsId;
         if (paramsId == 1) {
+          this.title ="Sélection";
           this.getProducts('/products/search/selectedProducts');
         } else if (paramsId == 2) {
-          let idCat = this.route.snapshot.params.paramsId2;
-          this.getProducts('/categories/' + idCat + '/products');
+            let idCat = this.route.snapshot.params.paramsId2;
+            this.title ="Produits de la catégorie "+idCat;
+            this.getProducts('/categories/' + idCat + '/products');
+        } else if (paramsId == 3) {
+            this.title ="Produits en promotion";
+            this.getProducts('/products/search/promoProducts');
+        } else if (paramsId == 4) {
+            this.title ="Produits Disponibles";
+            this.getProducts('/products/search/dispoProducts');
+        } else if (paramsId == 5) {
+            this.title ="Recherche...";
+            this.getProducts('/products/search/dispoProducts');
         }
       }
     });
@@ -39,6 +58,32 @@ export class ProductsComponent implements OnInit {
       }, err=>{
         console.log(err);
       });
+  }
+
+  onEditPhoto(p) {
+    this.currentProduct = p;
+    this.editPhoto = true;
+  }
+
+  //upload de fichier
+  onSelectedFile(event) {
+    this.selectedFiles = event.target.files;
+  }
+
+  uploadPhoto() {
+    this.progress = 0;
+    this.currentFileUpload = this.selectedFiles.item(0);
+    this.catService.uploadPhotoProduct(this.currentFileUpload, this.currentProduct.id).subscribe(event =>{
+      if (event.type === HttpEventType.UploadProgress) {
+        this.progress = Math.round(100 * event.loaded / event.total);
+      } else if (event instanceof HttpResponse){
+        this.toastr.success('Fin du téléchargement...', 'Succès!');
+        this.getProducts('/products/search/selectedProducts');
+      }
+    }, err => {
+      this.toastr.error('Problème de chargement', 'Erreur!');
+    });
+    this.selectedFiles = undefined;
   }
 
 }
